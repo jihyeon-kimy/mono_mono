893c4c86df5d8e8e499445582295374fe247af3a
jihyeon-kimy <jihyeon.rem@gmail.com>
2025-05-10 09:23:38 +0900

    프로젝트 세팅

---
+++ packages/ui/src/Flex/index.tsx
@@ -0,0 +1,31 @@
+import classNames from 'classnames/bind'
+import styles from './flex.module.scss'
+import type {HTMLAttributes} from 'react'
+
+const cx = classNames.bind(styles)
+
+export interface FlexProps extends HTMLAttributes<HTMLDivElement> {
+    children: React.ReactNode
+    direction?: 'row' | 'column'
+    justify?: 'start' | 'center' | 'end' | 'between' | 'around' | 'evenly'
+    align?: 'start' | 'center' | 'end'
+    gap?: 'none' | 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+}
+
+export function Flex({children, direction, justify, align, gap, className, ...props}: FlexProps) {
+    return (
+        <div
+            className={cx(
+                'flex',
+                direction && `direction-${direction}`,
+                justify && `justify-${justify}`,
+                align && `align-${align}`,
+                gap && `gap-${gap}`,
+                className,
+            )}
+            {...props}
+        >
+            {children}
+        </div>
+    )
+}
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,8 @@
+export * from './Text'
+export * from './Button'
+export * from './Flex'
+export * from './Grid'
+export * from './colors'
+export * from './Image'
+
+export const foo = 'foo'
